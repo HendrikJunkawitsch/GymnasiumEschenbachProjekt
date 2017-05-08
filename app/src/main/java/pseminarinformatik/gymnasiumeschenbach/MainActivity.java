5a285fe085b5baba39c157b26eb3e3b94433954d
HendrikJunkawitsch <sordes.derisor@googlemail.com>
2017-05-08 16:57:53 +0200

    Commit Message

--- app/src/main/java/pseminarinformatik/gymnasiumeschenbach/MainActivity.java
+++ app/src/main/java/pseminarinformatik/gymnasiumeschenbach/MainActivity.java
@@ -3,11 +3,13 @@ package pseminarinformatik.gymnasiumeschenbach;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+public class MainActivity extends AppCompatActivity
+{
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Penis
     }
 }
