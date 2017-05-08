28532ff69630064c51dea0c04a490f1431ec7455
Philipp <phillianus@gmail.com>
2017-05-08 17:19:32 +0200

    123

--- app/src/main/java/pseminarinformatik/gymnasiumeschenbach/MainActivity.java
+++ app/src/main/java/pseminarinformatik/gymnasiumeschenbach/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //pssssst hendrik is hässlicher
+        //pssssst hendrik is hässlicherer
     }
 }
